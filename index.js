7b2b9f310fe204f718940b0fb49a55ceb6870a71
Crystal <crystal120599@gmail.com>
2022-02-25 14:24:19 -0800

    UpdateD

--- index.js
+++ index.js
@@ -1,47 +1,63 @@
-async function pokedex() {
-    let pokedex = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`);
-    let data = await pokedex.json();
-    //console.log(data)
-    data.results.forEach(pokemon => {
-        //console.log(pokemon);
-        let pokemonName = pokemon.name;
-        //console.log(pokemonName);
-        let pokemonUrl = pokemon.url;
-        //console.log(pokemonUrl);
-        grabPokemon(pokemonUrl)
-    });
-}
+window.addEventListener("DOMContentLoaded", ()=>{
+    async function pokedex() {
+        let pokedex = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`);
+        let data = await pokedex.json();
+        //console.log(data)
+        data.results.forEach(pokemon => {
+            //console.log(pokemon);
+            let pokemonName = pokemon.name;
+            //console.log(pokemonName);
+            let pokemonUrl = pokemon.url;
+            //console.log(pokemonUrl);
+            grabPokemon(pokemonUrl)
+        });
+    }
 
-async function grabPokemon(url){
-    const pokeDiv = document.createElement("div");
-    let pokename = document.createElement("h3");
-    let pokeNumber = document.createElement("p");
-    pokeNumber.setAttribute("class", "dex-number")
-    let pokeimage = document.createElement("img");
-    let pokeTypeDiv = document.createElement("div");
-    let pokeType = document.createElement("p")
+    async function grabPokemon(url){
+        const pokeDiv = document.createElement("div");
+        let pokename = document.createElement("h3");
+        let pokeNumber = document.createElement("p");
+        pokeNumber.setAttribute("class", "dex-number")
+        let pokeimage = document.createElement("img");
+        const pokeTypeDiv = document.createElement("ul");
+        let pokeType;
 
-    pokeDiv.setAttribute("class", "pokemon")
-    document.body.appendChild(pokeDiv)
-    pokeDiv.appendChild(pokename)
-    pokeDiv.appendChild(pokeNumber)
-    pokeDiv.appendChild(pokeimage)
-    pokeDiv.appendChild(pokeTypeDiv)
-    pokeTypeDiv.appendChild(pokeType);
+        pokeDiv.setAttribute("class", "pokemon")
+        pokeTypeDiv.setAttribute("class","type")
+        document.body.appendChild(pokeDiv)
+        pokeDiv.appendChild(pokename)
+        pokeDiv.appendChild(pokeNumber)
+        pokeDiv.appendChild(pokeimage)
+        pokeDiv.appendChild(pokeTypeDiv)
 
-    let pokemon = await fetch(url);
-    let data = await pokemon.json();
-    console.log(data);
-    pokeNumber.innerText = `#${data.id}`;
-    const pokeName = data.name;
-    pokename.innerText = pokeName;
-    pokeDiv.setAttribute("id", pokeName)
-    //console.log(pokeName);
-    pokeimage.src = data.sprites.front_default;
-    data.types.forEach(type=>{
-        pokeType.innerText = type.type.name;
-        console.log(type.type.name)
-    })
-}
 
-pokedex();
+        let pokemon = await fetch(url);
+        let data = await pokemon.json();
+        //console.log(data);
+        pokeNumber.innerText = `#${data.id}`;
+        const pokeName = data.name;
+        pokename.innerText = pokeName;
+        pokeDiv.setAttribute("id", pokeName)
+        //console.log(pokeName);
+        pokeimage.src = data.sprites.front_default;
+
+        //console.log(data.types);
+        let arrayType = data.types;
+        for(let i = 0; i < arrayType.length; i++){
+            let type = arrayType[i].type.name;
+            //console.log(type);
+            pokeType = document.createElement("li");
+            pokeType.setAttribute("class",type)
+            pokeType.innerText = type;
+            pokeTypeDiv.appendChild(pokeType)
+        }
+
+
+
+
+    }
+
+    pokedex();
+
+
+})
